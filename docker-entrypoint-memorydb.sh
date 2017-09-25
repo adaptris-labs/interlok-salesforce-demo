847e83a96d88216e9af2fd3b084107b5e806e697
Lewin Chan <lewin.chan@adaptris.com>
2017-09-25 16:26:00 +0100

    Removed redundant properties.

--- docker-entrypoint-memorydb.sh
+++ docker-entrypoint-memorydb.sh
@@ -1,13 +1,5 @@
 #! /bin/sh
 
-set -e
-
-INTERLOK_ENV=${INTERLOK_ENV:="development"}
-
-export ADAPTER_PATH=./config/adapter.xml
-export MANAGEMENT_COMPONENTS=activemq:jmx:jetty
-export JMX_URL=service:jmx:activemq:///tcp://localhost:61616?jmx.destination=web-adapter
-
 cat > /opt/interlok/ui-resources/interlokuidb.properties<<EOL
 dataSource.provider=derby
 dataSource.driverClass=org.apache.derby.jdbc.EmbeddedDriver
